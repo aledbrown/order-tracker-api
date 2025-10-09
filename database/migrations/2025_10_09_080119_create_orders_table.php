77b57133c08adbff4e4c42c556dae033a8246dbe
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-10-09 09:04:05 +0100

    Populate migrations with fields

--- database/migrations/2025_10_09_080119_create_orders_table.php
+++ database/migrations/2025_10_09_080119_create_orders_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
+            $table->date('order_date');
             $table->timestamps();
         });
     }
